6298e7f9352a1b591ce4fa2d896347b2f6779aaa
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-11-12 15:30:00 -0500

    Update download.R

--- download.R
+++ download.R
@@ -1,3 +1,7 @@
-library(downloader)
-## 20Q2
-download("https://ndownloader.figshare.com/files/22629110",destfile="/pfs/out/CCLE_mutations.csv")
+### Here we download the broad depmap processing for mutations, and the sample info from their website. This is Public20q3.
+
+outdir <- "/pfs/out"
+
+download.file("https://ndownloader.figshare.com/files/24613355", destfile = file.path(outdir, "CCLE_mutations.csv"))
+
+download.file("https://ndownloader.figshare.com/files/24613394", destfile = file.path(outdir, "sample_info.csv"))
